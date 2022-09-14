d04ce3fa09921de617fb3ce303419da9cca8d384
Alicecoder454 <mwendealice454@gmail.com>
2022-09-14 15:06:26 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifindef HOLBERTON_H_INCLUDED
+#ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
@@ -7,13 +7,14 @@ void print_alphabet_x10(void);
 int _lslower(int c);
 int _lsalpha(int c);
 int print_sign(int n);
-int_abs(int);
+int 
+int _abs(int);
 int print_last_digit(int);
-void jack_bauser(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int a);
 void num(int r, int n);
 
-#end it 
+#endif
