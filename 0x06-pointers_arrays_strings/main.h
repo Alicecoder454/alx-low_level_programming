4a39c7605ce0bb403fc75c1d4646b6df0492cda8
Alicecoder454 <mwendealice454@gmail.com>
2022-09-21 22:30:38 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,27 +1,23 @@
-#include "main.h"
-
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 /**
- * rot13 - encodes a string into rot13
- * @s: string to encode
- *
- * Return: address of s
+ *void prototypes()
+ *int prototypes()
+ *char prototypes()
  */
-char *rot13(char *s)
-{
-	int i, j;
-	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-	for (i = 0; *(s + i); i++)
-	{
-		for (j = 0; j < 52; j++)
-		{
-			if (a[j] == *(s + i))
-			{
-				*(s + i) = b[j];
-				break;
-			}
-		}
-	}
-	return (s);
-}
+#endif /*MAIN_H*/
